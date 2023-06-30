59372dd401ccf4548cf24ca462ddcda87679f00f
Matt Riley <m@ttriley.dev>
2023-06-30 22:09:38 +1000

    Unspecified changes

--- src/test-runner.js
+++ src/test-runner.js
@@ -1,24 +1,29 @@
-const path = require('path');
 const process = require('process');
+const path = require('path');
+const fs = require('fs');
+
 const { main = 'index.js' } = require(path.resolve('package.json'));
+const mainPath = path.resolve(main);
 
 const defaultOptions = {
-    testTarget: require(path.resolve(main)),
-    testFiles: process.argv.slice(2).map(f => path.resolve(f)),
+    files: process.argv.slice(2).map(f => path.resolve(f)),
+    args: fs.existsSync(mainPath) ? [require(mainPath)] : [],
     test: require('node:test'),
-    assert: require('node:assert/strict')
+    assert: require('node:assert/strict'),
+    helpers: undefined,
+    window: globalThis.window
 };
 
 module.exports = () => (options = {}) => {
 
-    const { testTarget, testFiles, test, assert } = { ...defaultOptions, ...options };
+    const { files, args, test, assert, helpers, window } = { ...defaultOptions, ...options };
 
-    testFiles.forEach(f => {
-        const ini = require(f);
+    files.forEach(async f => {
+        const { default: ini } = await import(f);
         if (typeof ini !== 'function') return;
-        const run = ini({ test, assert }, testTarget);
+        const run = ini({ test, assert, helpers, window }, ...args);
         if (typeof run !== 'function') return;
-        run(testTarget);
+        run(...args);
     });
 
 };
