055752ceaf3f2ec04bed460b1f1e72e60776891b
Matt Riley <m@ttriley.dev>
2023-08-18 16:31:30 +1000

    Unspecified changes

--- src/test-runner.js
+++ src/test-runner.js
@@ -11,24 +11,24 @@ const defaultOptions = {
     args: []
 };
 
-const loadModule = modulePath => {
+const loadModuleDefault = async modulePath => {
     try {
         return require(modulePath);
     } catch (err) {
-        if (err.code === 'ERR_REQUIRE_ESM') return import(modulePath);
+        if (err.code === 'ERR_REQUIRE_ESM') return import(modulePath).then(m => m.default);
         throw err;
     }
 };
 
 const loadMain = () => {
     if (!main) return null;
-    return loadModule(path.resolve(main));
+    return loadModuleDefault(path.resolve(main));
 };
 
 const loadCompose = () => {
     const search = ['js', 'cjs', 'mjs'].map(ext => path.resolve(`./src/compose.${ext}`));
     const match = search.find(f => fs.existsSync(f));
-    return match ? loadModule(match) : null;
+    return match ? loadModuleDefault(match) : null;
 };
 
 module.exports = (options = {}) => {
@@ -36,7 +36,8 @@ module.exports = (options = {}) => {
     const { files, test, assert, context, args } = { ...defaultOptions, ...options };
 
     files.forEach(async f => {
-        const ini = await loadModule(f);
+        const ini = await loadModuleDefault(f);
+        console.warn(ini);
         if (typeof ini !== 'function') return;
         const compose = await loadCompose();
         const main = await loadMain();
